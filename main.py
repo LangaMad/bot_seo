585e2abe87836ae431abe493e4d0fb9e171d93aa
LangaMAd <artykovh@gmail.com>
2024-06-03 19:17:50 +0600

    update models & update main

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
+from config import TOKEN
+from database.models import models_main
+import asyncio
 
 
+async def main():
+    await models_main()
 
 
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except Exception as e:
+        print("Ошибка",e)
 
 
 
